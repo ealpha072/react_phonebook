19c62e9bba7c702a7b713a9217b2431faaf5c5d6
ealpha072 <ealpha072@gmail.com>
2022-12-08 23:12:45 +0300

    Boilerplate code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,29 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Contact from './Components/Contact'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = ({contacts}) => {
+  const [contact, setContact] = useState(contacts)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1>Alpha Phonebook</h1>
+
+      <div className="input-contacts">
+        <form action="">
+          <div className="input-fields">
+            <input type="text" placeholder='Input name'/>
+            <input type="number" placeholder='Phone NUmber'/>
+          </div>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="container">
+        <ul>
+          {contacts.map(contact => 
+            <Contact key={contact.id} contact = {contact}/>)
+          }
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './App.css'
+
+const contacts = [
+  {
+    id: 1,
+    name: 'ALpha Emmanuel',
+    number: '0798975799',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App contacts={contacts}/>
   </React.StrictMode>
 )
