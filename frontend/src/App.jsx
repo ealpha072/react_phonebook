2c5466bc71bc3a19e89f4d4adf9f63b839860aed
ealpha072 <ealpha072@gmail.com>
2022-12-12 22:14:12 +0300

    Forms added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,24 +2,73 @@ import React from 'react'
 import { useState } from 'react'
 import Contact from './Components/Contact'
 
-const App = ({contacts}) => {
-  const [contact, setContact] = useState(contacts)
+const App = (props) => {
+
+  const initialFormValues = {
+    name: '',
+    number:''
+  }
+
+  const [contacts, setContacts] = useState(props.contacts)
+  const [values, setValues] = useState(initialFormValues)
+  const [showAll, setShowAll] = useState(true)
+
+  const contactsToShow = showAll ? contacts : contacts.filter(contact => contact.important == true)
+  const addContact = (e) => {
+    e.preventDefault()
+    console.log('Form submitted')
+    const contactObject = {
+      id:contacts.length + 1,
+      name: values.name,
+      number:values.number,
+      date:new Date().toISOString(),
+      important:Math.random() < 0.5
+    }
+
+    console.log(contactObject)
+    setContacts(contacts.concat(contactObject))
+    setValues(initialFormValues)
+  }
+
+  const handleInputChange = (event) => {
+    const {name, value} = event.target
+    setValues({
+      ...values,
+      [name]:value
+    })
+  }
 
   return (
     <div>
       <h1>Alpha Phonebook</h1>
+      <button onClick={()=>setShowAll(false)}>Show {!showAll ? 'All' : 'Important'}</button>
 
-      <div className="input-contacts">
-        <form action="">
+      <div className="input-contacts" >
+        <form action="" onSubmit={addContact}>
           <div className="input-fields">
-            <input type="text" placeholder='Input name'/>
-            <input type="number" placeholder='Phone NUmber'/>
+            <input 
+              type="text" 
+              placeholder='Input name' 
+              value={values.name}
+              name='name' 
+              onChange={handleInputChange} 
+            />
+
+            <input 
+              type="number" 
+              placeholder='Phone number' 
+              value={values.number} 
+              name='number'
+              onChange={handleInputChange} 
+            />
+
           </div>
+          <button type='submit' >Add</button>
         </form>
       </div>
       <div className="container">
         <ul>
-          {contacts.map(contact => 
+          {contactsToShow.map(contact => 
             <Contact key={contact.id} contact = {contact}/>)
           }
         </ul>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,10 +6,24 @@ import './App.css'
 const contacts = [
   {
     id: 1,
-    name: 'ALpha Emmanuel',
+    name: 'Alpha Emmanuel',
     number: '0798975799',
     date: '2019-05-30T17:30:31.098Z',
     important: true
+  },
+  {
+    id: 2,
+    name: 'Corazone Kemunto',
+    number: '079675466',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 3,
+    name: 'David Omollo',
+    number: '0794565312',
+    date: '2019-05-30T17:30:31.098Z',
+    important: false
   },
 ]
 
